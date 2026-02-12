84e7f2b751523381286938f8bd274d87ec5b0ee9
aarav.muni <aarav.muni@gmail.com>
2026-02-12 13:47:53 +0530

    displaying graphs, 2 strategies

--- files/main.py
+++ files/main.py
@@ -2,35 +2,58 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-def main():
-    data = yf.download("AAPL","2025-11-01","2025-12-31")
-    print("data:")
-    print(data)
-    price = data.dropna()
-    print("Signals:")
-    price["Short Avg"] = price["Close"].rolling(5).mean()
-    price["Long Avg"] = price["Close"].rolling(10).mean()
-    price["Signal"] = (price["Short Avg"] > price["Long Avg"]).astype(int) * price["Close"]["AAPL"]
-    print(price)
+def calc_returns(data,close):
+    data["Returns"] = close.pct_change()
+    data["Cumulative_Market"] = (data["Returns"]+1).cumprod()
 
-    plt.figure(figsize=(12,6))
+def run_strategy_and_plot(strat,data,stock):
 
-    plt.scatter(price.index,price["Signal"])
-    plt.plot(price.index,price["Close"])
-    plt.plot(price.index,price["Short Avg"])
-    plt.plot(price.index,price["Long Avg"])
-    
-    plt.xlabel("Date")
-    plt.ylabel("close and signal")
-    plt.title("signals from close price")
-    plt.legend()
-    plt.grid(True)
-    
-    plt.savefig("plot.png", dpi=300, bbox_inches="tight")
-    plt.close()
+    close = data[("Close",stock)]
+    opn = data[("Open",stock)]
+    high = data[("High",stock)]
+    low = data[("Low",stock)]
+    volume = data[("Volume",stock)]
+
+    name = strat(data,close,opn,high,low,volume)
+    data["Strategy Returns"] = data["Signal"].shift(1)*data["Returns"]
+    data["Cumulative_Strategy"] = (data["Strategy Returns"]+1).cumprod()
 
 
+    data[["Cumulative_Market","Cumulative_Strategy"]].plot()
+    plt.title(f"{name}__{stock}")
 
-if __name__ == "__main__":
-    main()
+    os.makedirs("Graphs_By_Strategy",exist_ok=True)
+    os.makedirs(f"Graphs_By_Strategy/{name}",exist_ok=True)
+    plt.savefig(f"Graphs_By_Strategy/{name}/{name}__{stock}",format="png")
+    os.makedirs("Graphs_By_Stock",exist_ok=True)
+    os.makedirs(f"Graphs_By_Stock/{stock}",exist_ok=True)
+    plt.savefig(f"Graphs_By_Stock/{stock}/{name}__{stock}",format="png")
+
+    print(f"SAVED PLOT:{name}__{stock}.png\tRESULT:{data["Cumulative_Strategy"].iloc[-1]/data["Cumulative_Market"].iloc[-1]}")
+
+#strategies
+
+def moving_average(data,close,opn,high,low,volume):
+    data["Signal"] = np.where(close.rolling(20).mean() > close.rolling(50).mean(),1,0)
+    return "MOVING_AVERAGE_20-50"
+
+def moving_average_shorting(data,close,opn,high,low,volume):
+    data["Signal"] = np.where(close.rolling(20).mean() > close.rolling(50).mean(),1,-1)
+    return "MOVING_AVERAGE_SHORT_20-50"
+
+#extracting data
+tickers = ("AAPL","NVDA","GOOGL","MSFT")
+data = yf.download(tickers,"2023-01-01","2026-01-01")
+
+if data is None or data.empty:
+    print("Download failed.")
+    exit()
+
+strategies = [moving_average, moving_average_shorting]
+
+for stock in tickers:
+    calc_returns(data,(data[("Close",stock)]))
+    for strat in strategies:
+        run_strategy_and_plot(strat,data,stock)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from tradingtest!")
-
-
-if __name__ == "__main__":
-    main()
